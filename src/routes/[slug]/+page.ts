42caaed4d9a5d72254d3713cf138f94ab7585779
Mossati <xmossati9786@gmail.com>
2025-03-29 16:03:46 +0100

    fix: posts

--- src/routes/[slug]/+page.ts
+++ src/routes/[slug]/+page.ts
@@ -3,25 +3,15 @@ import { error } from '@sveltejs/kit'
 export async function load({ params }) {
 	try {
 		let post;
-		const paths = ['../../posts/', '../../posts_wlw/', '../../posts_experiments/'];
 
-        // try {
-        //     post = await import(`../../posts/${params.slug}.md`);
-        // } catch {
-        //     post = await import(`../../posts_wlw/${params.slug}.md`);
-        // }
-
-		for (const path of paths) {
+        try {
+            post = await import(`../../posts/${params.slug}.md`);
+        } catch {
             try {
-                post = await import(`${path}${params.slug}.md`);
-                break; // Exit the loop if the file is found
-            } catch (e) {
-                console.log(e);
+                post = await import(`../../posts_experiments/${params.slug}.md`);
+            }catch {
+                post = await import(`../../posts_wlw/${params.slug}.md`);
             }
-        }
-
-		if (!post) {
-            throw error(404, `Could not find ${params.slug}`);
         }
 
 		return {
